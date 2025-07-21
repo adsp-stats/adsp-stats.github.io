8838fda5b2c589cc01d666b97d1a2e47ac466d7a
Jonathan Williams <cthonathon@gmail.com>
2025-07-21 12:23:12 -0500

    continued chapter 1 changes

--- .quarto/_webr/appdir/app.R
+++ .quarto/_webr/appdir/app.R
@@ -2,21 +2,20 @@ library(shiny)
 library(bslib)
 
 ui <- page_fluid(
-      tags$head(tags$style(HTML("body {overflow-x: hidden;}"))),
-  title = "Normal distribution PDF",
-  fluidRow(plotOutput("distPlot")),
-  fluidRow(column(width=6,sliderInput("mu", "Mean (mu)", min=-10, max=10, value=0)),
-           column(width=6,sliderInput("sigma", "Std Dev (sigma)", min=0.01, max=10, value=1))))
+  tags$head(tags$style(HTML("body {overflow-x: hidden;}"))),
+  title = "Variance and bias of Normal estimators",
+  fluidRow(column(width=3,""),column(width=6,sliderInput("nsamp", "N (sample size)", min=5, max=200, value=25)),column(width=3,"")),
+  fluidRow(column(width=12,plotOutput("distPlot1"))),
+  fluidRow(column(width=12,plotOutput("distPlot2"))))
 
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- seq(input$mu-3*input$sigma,input$mu+3*input$sigma,input$sigma/100)
-    y <- dnorm(x,input$mu,input$sigma)
-    xlims <- c(mean(c(-3,x[1])),mean(c(3,x[601])))
-    ylims <- c(0,mean(c(dnorm(0),y[301])))
-    plot(x=x,y=y,main=NULL,xlab='x',ylab='Density',type='l',lwd=2,
-         xlim=xlims,ylim=ylims)
-  })
+  x <- reactive({matrix(rnorm(10000*input$nsamp,100,20),nrow=10000)})
+  xbar <- reactive({apply(x(),1,mean)})
+  xmed <- reactive({apply(x(),1,median)})
+  xsd <- reactive({apply(x(),1,sd)})
+  xrmse <- reactive({sqrt(xsd()^2*(input$nsamp-1)/input$nsamp)})
+  output$distPlot1 <- renderPlot({hist(xbar(), breaks=seq(min(xbar(),xmed()),max(xbar(),xmed()),length.out=31), main='10,000 estimates for the mean', xlab='Mu-hat', ylab='Frequency', col='#ff000080'); hist(xmed(), breaks=seq(min(xbar(),xmed()),max(xbar(),xmed()),length.out=31), col='#0000ff80', add=TRUE); legend(x='topright', legend=c('Sample mean','Sample median'), fill=c('#ff000080','#0000ff80'), bty='n')})
+  output$distPlot2 <- renderPlot({hist(xrmse(), breaks=seq(min(xrmse(),xsd()),max(xrmse(),xsd()),length.out=31), main='10,000 estimates for the std dev', xlab='Sigma-hat', ylab='Frequency', col='#ff000080'); hist(xsd(), breaks=seq(min(xrmse(),xsd()),max(xrmse(),xsd()),length.out=31), col='#0000ff80', add=TRUE); legend(x='topright', legend=c('Uncorrected: /(n)','Corrected: /(n-1)'), fill=c('#ff000080','#0000ff80'), bty='n')})
 }
 
 shinyApp(ui = ui, server = server)
