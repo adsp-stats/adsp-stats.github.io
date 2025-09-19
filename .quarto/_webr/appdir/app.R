fa4005d77aea6ca93532d815ba4fd8c7888da604
Jonathan Williams <cthonathon@gmail.com>
2025-09-19 11:16:37 -0500

    adding more chapters, trying to fix RShiny issues

--- .quarto/_webr/appdir/app.R
+++ .quarto/_webr/appdir/app.R
@@ -1,21 +1,27 @@
 library(shiny)
 library(bslib)
 
+set.seed(1628)
+hetdat <- data.frame(X=runif(200,min=0,max=10))
+hetdat$Y <- 10 + 1.5*hetX + 0.5*hetX*rnorm(200)
+hetLM <- lm(Y~X,hetdat)
+hetint <- predict(hetLM,newdata=data.frame(X=seq(0,10,0.1)),
+                  interval='prediction',level=0.8)
+  
 ui <- page_fluid(
   tags$head(tags$style(HTML("body {overflow-x: hidden;}"))),
-  title = "Variance and bias of Normal estimators",
-  fluidRow(column(width=3,""),column(width=6,sliderInput("nsamp", "N (sample size)", min=5, max=200, value=25)),column(width=3,"")),
-  fluidRow(column(width=12,plotOutput("distPlot1"))),
-  fluidRow(column(width=12,plotOutput("distPlot2"))))
+  title = "80% CIs for predicted new observations",
+  fluidRow(sliderInput("x", "x-level of new observations", min=0, max=10, value=5, step=0.1)),
+  fluidRow(plotOutput("distPlot1")))
 
 server <- function(input, output) {
-  x <- reactive({matrix(rnorm(10000*input$nsamp,100,20),nrow=10000)})
-  xbar <- reactive({apply(x(),1,mean)})
-  xmed <- reactive({apply(x(),1,median)})
-  xsd <- reactive({apply(x(),1,sd)})
-  xrmse <- reactive({sqrt(xsd()^2*(input$nsamp-1)/input$nsamp)})
-  output$distPlot1 <- renderPlot({hist(xbar(), breaks=seq(min(xbar(),xmed()),max(xbar(),xmed()),length.out=31), main='10,000 estimates for the mean', xlab='Mu-hat', ylab='Frequency', col='#ff000080'); hist(xmed(), breaks=seq(min(xbar(),xmed()),max(xbar(),xmed()),length.out=31), col='#0000ff80', add=TRUE); legend(x='topright', legend=c('Sample mean','Sample median'), fill=c('#ff000080','#0000ff80'), bty='n')})
-  output$distPlot2 <- renderPlot({hist(xrmse(), breaks=seq(min(xrmse(),xsd()),max(xrmse(),xsd()),length.out=31), main='10,000 estimates for the std dev', xlab='Sigma-hat', ylab='Frequency', col='#ff000080'); hist(xsd(), breaks=seq(min(xrmse(),xsd()),max(xrmse(),xsd()),length.out=31), col='#0000ff80', add=TRUE); legend(x='topright', legend=c('Uncorrected: /(n)','Corrected: /(n-1)'), fill=c('#ff000080','#0000ff80'), bty='n')})
+  index <- reactive({input$x*10+1})
+  output$distPlot1 <- renderPlot({
+    plot(NA,NA,xlab='X',ylab='Y',xlim=c(-0.1,10.1),ylim=c(0,40))
+    rect(xleft=input$x-0.1,ybottom=hetint[index(),2],
+         xright=input$x+0.1,ytop=hetint[index(),3],
+         border=NA,density=NA,col='#0000ff50')
+    points(hetdat$X,hetdat$Y,)})
 }
 
 shinyApp(ui = ui, server = server)
